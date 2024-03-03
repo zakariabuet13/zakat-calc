97d1076f0d13f8b06d489a813ed32069fec2f215
Zakaria Ahmed <zakaria.buet13@gmail.com>
2024-03-03 11:22:53 +0600

    UI building

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,18 +4,26 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <Box py={2} sx={{ bgcolor: "secondary.main" }}>
+    <Box py={2} mb={4} sx={{ bgcolor: "secondary.main", position: "sticky", top: 0 }}>
       <Container maxWidth={"lg"} sx={{ display: "flex" }} className={styles.header}>
         <Link to={"/"}>
           <Box className={styles.logo}>
-            <Typography variant="h4">যাকাত</Typography>
-            <Typography variant="h5">ক্যালকুলেটর</Typography>
+            <Typography variant="h4" color={"primary.main"}>
+              যাকাত
+            </Typography>
+            <Typography variant="h5" color={"primary.main"}>
+              ক্যালকুলেটর
+            </Typography>
           </Box>
         </Link>
 
         <Box className={styles.menu}>
-          <Link to={"/"} className={styles["menu-item"]}>ক্যালকুলেটর</Link>
-          <Link to={"/about-zakat"} className={styles["menu-item"]}>যাকাত সম্পর্কে</Link>
+          <Link to={"/"} >
+            <Typography className={styles["menu-item"]} color={"primary.main"}>ক্যালকুলেটর</Typography>
+          </Link>
+          <Link to={"/about-zakat"}>
+            <Typography className={styles["menu-item"]} color={"primary.main"}>যাকাত সম্পর্কে</Typography>
+          </Link>
         </Box>
       </Container>
     </Box>
